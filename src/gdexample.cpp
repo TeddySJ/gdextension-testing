8e798c895bcda72b6951dc53e0eb24f7e27f0467
Teddy Sjöström <teddytyler@gmail.com>
2025-03-14 09:36:43 +0100

    Slightly modified the extension to also have a Vector2 that moves the Godot Icon along at a user-defined velocity

--- src/gdexample.cpp
+++ src/gdexample.cpp
@@ -4,11 +4,23 @@
 using namespace godot;
 
 void GDExample::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("get_amplitude"), &GDExample::get_amplitude);
+	ClassDB::bind_method(D_METHOD("set_amplitude", "p_amplitude"), &GDExample::set_amplitude);
+
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "amplitude"), "set_amplitude", "get_amplitude");
+
+	ClassDB::bind_method(D_METHOD("get_velocity"), &GDExample::get_velocity);
+	ClassDB::bind_method(D_METHOD("set_velocity", "p_velocity"), &GDExample::set_velocity);
+
+	ADD_PROPERTY(PropertyInfo(Variant::VECTOR2, "velocity"), "set_velocity", "get_velocity");
+
 }
 
 GDExample::GDExample() {
 	// Initialize any variables here.
 	time_passed = 0.0;
+	amplitude = 10.0;
+	velocity = Vector2(0, 0);
 }
 
 GDExample::~GDExample() {
@@ -18,7 +30,28 @@ GDExample::~GDExample() {
 void GDExample::_process(double delta) {
 	time_passed += delta;
 
-	Vector2 new_position = Vector2(10.0 + (10.0 * sin(time_passed * 2.0)), 10.0 + (10.0 * cos(time_passed * 1.5)));
+	Vector2 new_position = Vector2(
+		amplitude + (amplitude * sin(time_passed * 2.0)),
+		amplitude + (amplitude * cos(time_passed * 1.5))
+	);
+
+	new_position += time_passed * velocity;
 
 	set_position(new_position);
+}
+
+void GDExample::set_amplitude(const double p_amplitude) {
+	amplitude = p_amplitude;
+}
+
+double GDExample::get_amplitude() const {
+	return amplitude;
+}
+
+void GDExample::set_velocity(const Vector2 p_velocity) {
+	velocity = p_velocity;
+}
+
+Vector2 GDExample::get_velocity() const {
+	return velocity;
 }
--- src/gdexample.hpp
+++ src/gdexample.hpp
@@ -8,11 +8,20 @@ namespace godot {
 class GDExample : public Sprite2D {
 	GDCLASS(GDExample, Sprite2D)
 
+protected:
+    static void _bind_methods();
+
 private:
 	double time_passed;
+	double amplitude;
+	Vector2 velocity;
 
-protected:
-	static void _bind_methods();
+public:
+	void set_amplitude(const double p_amplitude);
+	double get_amplitude() const;
+
+	void set_velocity(const Vector2 p_velocity);
+	Vector2 get_velocity() const;
 
 public:
 	GDExample();
